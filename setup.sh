b5b74aaeb9fee248f504e46993d503edcd37c2b9
Dinos Christou <dinos.christou@gmail.com>
2026-03-02 13:38:07 +0000

    Fix folders

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "🚀 Setting up dotfiles..."
+echo "Setting up dotfiles..."
 
 # Create necessary directories
-echo "📁 Creating directories..."
+echo "Creating directories..."
 mkdir -p ~/.config
 mkdir -p ~/.local/share/applications
 mkdir -p ~/.local/share/wallpapers
@@ -14,8 +14,12 @@ mkdir -p ~/.local/share/icons
 mkdir -p ~/.local/share/fonts
 
 # Symlink configurations
-echo "🔗 Symlinking configurations..."
+echo "Symlinking configurations..."
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Link bin folder
+echo "Linking bin folder..."
+ln -sf "$DOTFILES_DIR/bin" ~/
 
 # Shell configs
 ln -sf "$DOTFILES_DIR/zshrc" ~/.zshrc
@@ -23,38 +27,41 @@ ln -sf "$DOTFILES_DIR/vimrc" ~/.vimrc
 ln -sf "$DOTFILES_DIR/p10k.zsh" ~/.p10k.zsh
 ln -sf "$DOTFILES_DIR/gitconfig" ~/.gitconfig
 
-# XDG configs
+# Configs for applications
 ln -sf "$DOTFILES_DIR/config/nvim" ~/.config/
 ln -sf "$DOTFILES_DIR/config/kitty" ~/.config/
-ln -sf "$DOTFILES_DIR/config/hypr" ~/.config/
-ln -sf "$DOTFILES_DIR/config/waybar" ~/.config/
-ln -sf "$DOTFILES_DIR/config/wofi" ~/.config/
-ln -sf "$DOTFILES_DIR/config/mako" ~/.config/
 ln -sf "$DOTFILES_DIR/config/btop" ~/.config/
 ln -sf "$DOTFILES_DIR/config/lazygit" ~/.config/
 ln -sf "$DOTFILES_DIR/config/fastfetch" ~/.config/
 ln -sf "$DOTFILES_DIR/config/tmux" ~/.config/
 ln -sf "$DOTFILES_DIR/config/zsh" ~/.config/
 
-# Local data
-ln -sf "$DOTFILES_DIR/local/share/applications/"* ~/.local/share/applications/
-ln -sf "$DOTFILES_DIR/local/share/wallpapers/"* ~/.local/share/wallpapers/
-ln -sf "$DOTFILES_DIR/local/share/themes/"* ~/.local/share/themes/
-ln -sf "$DOTFILES_DIR/local/share/icons/"* ~/.local/share/icons/
-
 # Change default shell to zsh
-echo "🐚 Changing default shell to zsh..."
+echo "Changing default shell to zsh..."
 if [[ "$SHELL" != "$(which zsh)" ]]; then
     chsh -s $(which zsh)
 fi
 
 # Install vim plugins
-echo "📦 Installing Vim plugins..."
+echo "Installing Vim plugins..."
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 
+# Install tmux plugins
+echo "Installing tmux plugins..."
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# Install powerlevel10k
+echo "Installing Powerlevel10k..."
+if [ ! -d ~/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+fi
+
 echo "Setup complete!"
 echo ""
-echo "🔄 Please log out and log back in to complete the setup."
+echo "Please log out and log back in to complete the setup."
