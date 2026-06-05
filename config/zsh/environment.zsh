18f7c685b09239148db174c68777500301994fec
Dinos Christou <dinos.christou@gmail.com>
2026-06-05 14:51:55 +0100

    move antigravity stanzas

--- config/zsh/environment.zsh
+++ config/zsh/environment.zsh
@@ -10,18 +10,19 @@ PATH=/usr/local/go/bin:$PATH
 export PATH="~/go/bin:$PATH"
 
 # Antigravity CLI
-export PATH="~/.antigravity/antigravity/bin:$PATH"
+export PATH="$HOME/.antigravity/antigravity/bin:$PATH"
+export PATH="$HOME/.antigravity-ide/antigravity-ide/bin:$PATH"
 
 # Obsidian CLI
 export PATH="$PATH:/Applications/Obsidian.app/Contents/MacOS"
 
 # Infrastructure environment variables
 export SOPS_AGE_KEY_FILE=$HOME/.sops/key.txt
-export KUBECONFIG=~/.kube/config
+export KUBECONFIG=$HOME/.kube/config
 
 # Rust environment
-[ -r ~/.cargo/env ] && . ~/.cargo/env
+[ -r $HOME/.cargo/env ] && . $HOME/.cargo/env
 
 # GCP SDK
-[ -f ~/bin/google-cloud-sdk/path.zsh.inc ] && . ~/bin/google-cloud-sdk/path.zsh.inc
-[ -f ~/bin/google-cloud-sdk/completion.zsh.inc ] && . ~/bin/google-cloud-sdk/completion.zsh.inc
+[ -f $HOME/bin/google-cloud-sdk/path.zsh.inc ] && . $HOME/bin/google-cloud-sdk/path.zsh.inc
+[ -f $HOME/bin/google-cloud-sdk/completion.zsh.inc ] && . $HOME/bin/google-cloud-sdk/completion.zsh.inc
